0fcb6f61fa1a8d354b1dc811075e6936d53a3d8c
Utku Ulu <utku.ulu@bilgeadamboost.com>
2023-09-24 17:43:52 +0300

    Social Update

--- CalorieTrackingApp.UI/Soical.cs
+++ CalorieTrackingApp.UI/Soical.cs
@@ -135,18 +135,18 @@ namespace CalorieTrackingApp.UI
                     grp.Controls.Add(dateLabel);
 
                     //DeleteIcon
-                    if(post.AccountId == _account.Id)
+                    if (post.AccountId == _account.Id)
                     {
                         PictureBox picture2 = new PictureBox();
                         picture2.Image = Properties.Resources.xIcon;
                         picture2.SizeMode = PictureBoxSizeMode.StretchImage;
-                        picture2.Size = new Size(15,15);
+                        picture2.Size = new Size(15, 15);
                         picture2.Location = new Point(580, 17);
                         picture2.BringToFront();
                         picture2.Click += new EventHandler(picture2_Click);
                         picture2.Tag = post;
                         grp.Controls.Add(picture2);
-                        
+
                     }
 
                     //GroupBox
@@ -161,8 +161,8 @@ namespace CalorieTrackingApp.UI
         private void picture2_Click(object? sender, EventArgs e)
         {
             PictureBox pictureBox = sender as PictureBox;
-            DialogResult res =  MessageBox.Show("Post u silmek istediğinize emin misiniz?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if(res == DialogResult.Yes)
+            DialogResult res = MessageBox.Show("Post u silmek istediğinize emin misiniz?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
             {
                 SocialMediaPost post = pictureBox.Tag as SocialMediaPost;
                 posts.Delete(post);
